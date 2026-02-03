6fc6ab516b55d43d48c6fd9cb9e61fdba8777094
euangelica25 <angelicafelintodasilva@gmail.com>
2026-02-03 08:55:27 -0300

    criando classificador

--- classificador.js
+++ classificador.js
@@ -1,7 +1,96 @@
 //Definindo nicks e levels dos jogadores
 
-let nick1 = "euangelica25";
-let levelnick1 = 5390;
+let nick = "euangelica25";
+let levelnick = 5390;
 
+/*let nick2 = "ShadowRex";
+let levelnick2 = 11240;
 
+let nick3 = "LunaFox";
+let levelnick3 = 2840;
+
+let nick4 = "DarkWizard";
+let levelnick4 = 9780;
+
+let nick5 = "NeoHunter";
+let levelnick5 = 150;
+
+let nick6 = "PixelStorm";
+let levelnick6 = 6230;
+
+let nick7 = "IronClaw";
+let levelnick7 = 8450;
+
+let nick8 = "BluePhoenix";
+let levelnick8 = 4310;
+
+let nick9 = "NightCrawler";
+let levelnick9 = 11990;
+
+let nick10 = "StarGamer";
+let levelnick10 = 760;
+
+let nick11 = "VenomX";
+let levelnick11 = 10120;
+
+let nick12 = "FireSpark";
+let levelnick12 = 3890;
+
+let nick13 = "GhostByte";
+let levelnick13 = 6720;
+
+let nick14 = "OmegaKnight";
+let levelnick14 = 8900;
+
+let nick15 = "CyberWolf";
+let levelnick15 = 2140;
+
+let nick16 = "FrostBlade";
+let levelnick16 = 5580;
+
+let nick17 = "EchoSoul";
+let levelnick17 = 940;
+
+let nick18 = "ThunderXP";
+let levelnick18 = 7330;
+
+let nick19 = "VoidRunner";
+let levelnick19 = 11800;
+
+let nick20 = "RedComet";
+let levelnick20 = 4920;
+
+let nick21 = "MysticOne";
+let levelnick21 = 3650;
+
+let nick22 = "NovaStrike";
+let levelnick22 = 10750;
+
+let nick23 = "SilentArrow";
+let levelnick23 = 8200;
+
+let nick24 = "BitCrusher";
+let levelnick24 = 1290;
+
+let nick25 = "SolarFlare";
+let levelnick25 = 6840;
+
+let nick26 = "NightPixel";
+let levelnick26 = 2560;
+
+let nick27 = "AlphaZero";
+let levelnick27 = 12000;
+
+let nick28 = "MoonRider";
+let levelnick28 = 410;
+
+let nick29 = "DracoFire";
+let levelnick29 = 9600;
+
+let nick30 = "QuantumPlay";
+let levelnick30 = 5750;*/
+
+If levelnick < 1000 {
+    console.log(nick + "O Herói de nome" + nick + "está no nível de " + levelnick + " e é classificado como Ferro.");
+}
 
